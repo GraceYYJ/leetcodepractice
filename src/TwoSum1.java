c0be68bf55241df631351e1c2118b118f7c60659
GraceYYJ <649673739@qq.com>
2017-07-11 15:18:22 +0800

    the O(n) solution of twosum(hashmap)

--- src/TwoSum1.java
+++ src/TwoSum1.java
@@ -1,26 +1,32 @@
+import java.util.HashMap;
+
 /**
  * Created by Administrator on 2017/7/11.
  */
 public class TwoSum1 {
     public static void main(String args[]){
         TwoSum1 test=new TwoSum1();
-        int nums[]={3,3};
-        int result[]=test.twoSum(nums,6);
+        int nums[]={2,1,9,4,4};
+        int result[]=test.twoSum(nums,8);
         for(int i=0;i<result.length;i++){
             System.out.print(result[i]+" ");
         }
     }
-        public int[] twoSum(int[] nums, int target) {
-            int result[]=new int[2];
-            outer:for(int i=0;i<nums.length;i++){
-                inner:for(int j=i+1;j<nums.length;j++){
-                    if(nums[i]+nums[j]==target){
-                        result[0]=i;
-                        result[1]=j;
-                        break outer;
-                    }
-                }
+    public int[] twoSum(int[] nums, int target) {
+        int[] index=new int[]{0,1};
+        HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+        for(int i = 0; i<nums.length; i++)
+        {
+            if(hm.containsKey(target - nums[i]))
+            {
+                index[1] = i;
+                index[0] = hm.get(target-nums[i]);
+                return index;
+            }else
+            {
+                hm.put(nums[i],i);
             }
-            return result;
         }
+        return index;
+    }
 }
