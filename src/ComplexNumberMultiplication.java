4f2494f436192593bf79149990c45bc2f5811628
GraceYYJ <649673739@qq.com>
2017-08-29 17:23:59 +0800

    the O(n) solution of twosum(hashmap)

--- src/ComplexNumberMultiplication.java
+++ src/ComplexNumberMultiplication.java
@@ -3,17 +3,17 @@
  */
 public class ComplexNumberMultiplication {
     public String complexNumberMultiply(String a, String b) {
-        int q=Integer.parseInt(a.substring(0,1));
-        int w=Integer.parseInt(a.substring(2,3));
-        int e=Integer.parseInt(b.substring(0,1));
-        int r=a.charAt(2);
-        int x=Integer.parseInt(a.substring(0,1))*b.charAt(0)-a.charAt(2)*b.charAt(2);
-        int y=a.charAt(2)*b.charAt(0)+a.charAt(0)*b.charAt(2);
+        int q=Integer.parseInt(a.substring(0,a.indexOf("+")));
+        int w=Integer.parseInt(a.substring(a.indexOf("+")+1,a.indexOf("i")));
+        int e=Integer.parseInt(b.substring(0,b.indexOf("+")));
+        int r=Integer.parseInt(b.substring(b.indexOf("+")+1,b.indexOf("i")));
+        int x=q*e-w*r;
+        int y=w*e+q*r;
         System.out.println(x);
         System.out.println(y);
-        return String.valueOf(x)+"+"+String.valueOf(y);
+        return String.valueOf(x)+"+"+String.valueOf(y)+"i";
     }
     public static void main(String args[]){
-        System.out.println(new ComplexNumberMultiplication().complexNumberMultiply("1+1i","1+1i"));
+        System.out.println(new ComplexNumberMultiplication().complexNumberMultiply("2+-2i","1+1i"));
     }
 }
